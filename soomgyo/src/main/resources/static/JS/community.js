c2e4fd01396fd46fd699e7f51e197c3680e7e53f
임해원 <lhw931213@naver.com>
2022-12-19 12:44:18 +0900

    add spring community

--- soomgyo/src/main/resources/static/JS/community.js
+++ soomgyo/src/main/resources/static/JS/community.js
@@ -99,3 +99,30 @@ function hide5(){
 	
 	$(".menu5").css("background-color", "#ecd3ce");
 }
+
+let index = {
+	init: function(){
+		$("btn-save").on("click",()=>{
+			this.save();
+		});
+	},
+	save: function(){
+		let data={
+			category: $("#category").val(),
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+		$.ajax({
+			type:"POST",
+			url:"/api/board",
+			data:JSON.stringify(data),
+			contentType:"application/json; charset=utf-8",
+			dataType:"json"
+		}).done(function(resp){
+			location.href="/auth/community";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},
+}
+index.init();
