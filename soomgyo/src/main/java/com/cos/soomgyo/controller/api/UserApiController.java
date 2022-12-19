2f43b70aae2e5134f53c7ce52bfe9f29d5b4343c
giwan <elrmekekrmek@naver.com>
2022-12-19 13:15:49 +0900

    zz

--- soomgyo/src/main/java/com/cos/soomgyo/controller/api/UserApiController.java
+++ soomgyo/src/main/java/com/cos/soomgyo/controller/api/UserApiController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.soomgyo.dto.ResponseDto;
 import com.cos.soomgyo.model.Users;
 import com.cos.soomgyo.service.UserService;
 
+@RestController
 public class UserApiController {
 	@Autowired
 	private UserService userService;
